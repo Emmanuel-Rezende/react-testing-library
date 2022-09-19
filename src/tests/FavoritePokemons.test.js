2efb72f1dc50c317054a2a072c833fbe016c3d30
Emmanuel <emmanuel.redu.rezende@gmail.com>
2022-09-19 15:38:21 -0300

    Requisito 3 Striker

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+test(
+  'Exibir No favorite pokemon found, caso a pessoa não tenha pokémons favoritos',
+  () => {
+    renderWithRouter(<FavoritePokemons />);
+    const texto = screen.getByText('No favorite pokemon found');
+    expect(texto).toBeInTheDocument();
+  },
+);
