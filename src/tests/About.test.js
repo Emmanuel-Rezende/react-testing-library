d35fdec58b60f1fb69d29bf1a5a8c43c3d999029
Emmanuel <emmanuel.redu.rezende@gmail.com>
2022-09-19 15:30:25 -0300

    Requisito 2 Striker

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../pages/About';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste se a página contém as informações sobre a Pokédex', () => {
+  renderWithRouter(<About />);
+  const texto = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+  const imagem = screen.getByRole('img');
+  const linkImagem = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  expect(texto).toBeInTheDocument();
+  expect(imagem).toHaveAttribute('src', linkImagem);
+});
