1054899341aeeff68801cb0034431a586d71e726
Emmanuel <emmanuel.redu.rezende@gmail.com>
2022-09-19 15:05:15 -0300

    Requisito 1 Striker

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  renderWithRouter(<App />);
+  const home = screen.getByRole('link', { name: 'Home' });
+  const about = screen.getByRole('link', { name: 'About' });
+  const favorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(favorite).toBeInTheDocument();
+});
