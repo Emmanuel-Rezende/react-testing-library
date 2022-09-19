e7acc4a704d301c7de52c936c7b82098b0767b02
Emmanuel <emmanuel.redu.rezende@gmail.com>
2022-09-19 15:43:37 -0300

    Requisito 4 Striker

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+test('Teste se tem o texto Page requested not found e a imagem', () => {
+  renderWithRouter(<NotFound />);
+  const texto = screen.getByRole('heading', { name: 'Page requested not found' });
+  const imagem = screen.getByRole('img');
+  const linkImagem = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  expect(texto).toBeInTheDocument();
+  expect(imagem).toHaveAttribute('src', linkImagem);
+});
