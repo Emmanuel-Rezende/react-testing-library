2233490211bd3da473bce619ff6fcc1f080129f4
Emmanuel <emmanuel.redu.rezende@gmail.com>
2022-09-20 16:36:21 -0300

    Requisito 5 Striker

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+it('Testando os botoes de filtragem da Pokédex;:', () => {
+  renderWithRouter(<App />);
+  const botao = screen.getByRole('button', { name: /normal/i });
+  const botaoTipos = screen.getAllByTestId('pokemon-type-button');
+  const numero = 7;
+  const botaoAll = screen.getByRole('button', { name: /all/i });
+  expect(botao).toBeInTheDocument();
+  expect(botaoTipos).toHaveLength(numero);
+  expect(botaoAll).toBeInTheDocument();
+  userEvent.click(botaoAll);
+});
